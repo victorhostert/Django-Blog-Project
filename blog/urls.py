cfbe562ef249e868108b48f9e198d008004e162f
victorhostert <victorwhostert@gmail.com>
2021-11-22 14:00:17 -0300

    refactor: Refatoração do código

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,10 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('', views.blog),
-    path('about/', views.about),
-    path('<slug:slug>/', views.details),
+    path('', views.home, name='home'),
+    path('blog/', views.blog, name='blog'),
+    path('about/', views.about, name='about'),
+    path('<slug:slug>/', views.details, name='details'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
