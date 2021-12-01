665a02fa45574dcc8682bd60fb72179bcf1323ed
Victor Hostert <victorwhostert@gmail.com>
2021-12-01 17:22:53 -0300

    Trying to deploy to heroku

--- meusite/settings.py
+++ meusite/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import django_heroku
-from decouple import config
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
